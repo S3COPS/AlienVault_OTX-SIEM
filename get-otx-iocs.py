d04a2d02ba68a1661ebc5e57a6b4540006a17a08
S3COPS <kev@scranbag.com>
2017-06-08 23:18:22 +0100

    Create get-otx-iocs.py

--- get-otx-iocs.py
+++ get-otx-iocs.py
@@ -22,7 +22,7 @@ OTX_KEY = ''
 
 # ENTER ANY WHITELIST HASHES HERE:
 HASH_WHITELIST = ['e617348b8947f28e2a280dd93c75a6ad','125da188e26bd119ce8cad7eeb1fc2dfa147ad47',
-                  '06f7826c2862d184a49e3672c0aa6097b11e7771a4bf613ec37941236c1a8e20']
+				  '06f7826c2862d184a49e3672c0aa6097b11e7771a4bf613ec37941236c1a8e20']
 
 # ENTER ANY WHITELIST HASHES HERE:
 DOMAIN_WHITELIST = ['proofpoint.com']
@@ -31,185 +31,183 @@ class WhiteListedIOC(Exception): pass
 
 class OTXReceiver():
 
-    # IOC Strings
-    hash_iocs = ""
-    filename_iocs = ""
-    c2_iocs = ""
-    c2_domain_iocs = ""
-    c2_ip_iocs = ""
+	# IOC Strings
+	hash_iocs = ""
+	filename_iocs = ""
+	c2_iocs = ""
+	c2_domain_iocs = ""
+	c2_ip_iocs = ""
 
-    # Output format
-    separator = ";"
-    use_csv_header = False
-    extension = "txt"
-    hash_upper = False
-    filename_regex_out = True
+	# Output format
+	separator = ";"
+	use_csv_header = False
+	extension = "txt"
+	hash_upper = False
+	filename_regex_out = True
 
-    def __init__(self, api_key, siem_mode, debug, proxy):
-        self.debug = debug
-        self.otx = OTXv2(api_key, proxy)
-        if siem_mode:
-            self.separator = ","
-            self.use_csv_header = True
-            self.extension = "csv"
-            self.hash_upper = True
-            self.filename_regex_out = False
+	def __init__(self, api_key, siem_mode, debug, proxy):
+		self.debug = debug
+		self.otx = OTXv2(api_key, proxy)
+		if siem_mode:
+			self.separator = ","
+			self.use_csv_header = True
+			self.extension = "csv"
+			self.hash_upper = True
+			self.filename_regex_out = False
 
-    def get_iocs_last(self):
+	def get_iocs_last(self):
 
 # UNCOMMENT THE mtime LINE TO DOWNLOAD LAST n DAYS
-        # mtime = (datetime.now() - timedelta(days=days_to_load)).isoformat()
-        print "Starting OTX feed download ..."
-        self.events = self.otx.getall()
-        print "Download complete - %s events received" % len(self.events)
-        # json_normalize(self.events)
+		# mtime = (datetime.now() - timedelta(days=days_to_load)).isoformat()
+		print "Starting OTX feed download ..."
+		self.events = self.otx.getall()
+		print "Download complete - %s events received" % len(self.events)
+		# json_normalize(self.events)
 
-    def write_iocs(self, ioc_folder):
+	def write_iocs(self, ioc_folder):
 
-        hash_ioc_file = os.path.join(ioc_folder, "otx-hash-iocs.{0}".format(self.extension))
-        filename_ioc_file = os.path.join(ioc_folder, "otx-filename-iocs.{0}".format(self.extension))
-        c2_ioc_file = os.path.join(ioc_folder, "otx-c2-iocs.{0}".format(self.extension))
-        c2_domain_ioc_file = os.path.join(ioc_folder, "otx-c2-domain-iocs.{0}".format(self.extension))
-        c2_ip_ioc_file = os.path.join(ioc_folder, "otx-c2-ip-iocs.{0}".format(self.extension))
-                  
+		hash_ioc_file = os.path.join(ioc_folder, "otx-hash-iocs.{0}".format(self.extension))
+		filename_ioc_file = os.path.join(ioc_folder, "otx-filename-iocs.{0}".format(self.extension))
+		c2_ioc_file = os.path.join(ioc_folder, "otx-c2-iocs.{0}".format(self.extension))
+		c2_domain_ioc_file = os.path.join(ioc_folder, "otx-c2-domain-iocs.{0}".format(self.extension))
+		c2_ip_ioc_file = os.path.join(ioc_folder, "otx-c2-ip-iocs.{0}".format(self.extension))
+				  
 
-        print "Processing indicators ..."
-        for event in self.events:
-            try:
-                for indicator in event["indicators"]:
+		print "Processing indicators ..."
+		for event in self.events:
+			try:
+				for indicator in event["indicators"]:
 
-                    try:
-                        if indicator["type"] in ('FileHash-MD5', 'FileHash-SHA1', 'FileHash-SHA256'):
+					try:
+						if indicator["type"] in ('FileHash-MD5', 'FileHash-SHA1', 'FileHash-SHA256'):
 
-                            # Whitelisting
-                            if indicator["indicator"] in HASH_WHITELIST:
-                                raise WhiteListedIOC
+							# Whitelisting
+							if indicator["indicator"] in HASH_WHITELIST:
+								raise WhiteListedIOC
 
-                            hash = indicator["indicator"]
-                            if self.hash_upper:
-                                hash = indicator["indicator"].upper()
+							hash = indicator["indicator"]
+							if self.hash_upper:
+								hash = indicator["indicator"].upper()
 
-                            self.hash_iocs += "{0}{3}{1} {2}\n".format(
-                                hash,
-                                event["name"].encode('unicode-escape'),
-                                " / ".join(event["references"])[:500],
-                                self.separator)
+							self.hash_iocs += "{0}{3}{1} {2}\n".format(
+								hash,
+								event["name"].encode('unicode-escape'),
+								" / ".join(event["references"])[:500],
+								self.separator)
 
-                        if indicator["type"] == 'FilePath':
+						if indicator["type"] == 'FilePath':
 
-                            filename = indicator["indicator"]
-                            if self.filename_regex_out:
-                                filename = my_escape(indicator["indicator"])
+							filename = indicator["indicator"]
+							if self.filename_regex_out:
+								filename = my_escape(indicator["indicator"])
 
-                            self.filename_iocs += "{0}{3}{1} {2}\n".format(
-                                filename,
-                                event["name"].encode('unicode-escape'),
-                                " / ".join(event["references"])[:500],
-                                self.separator)
+							self.filename_iocs += "{0}{3}{1} {2}\n".format(
+								filename,
+								event["name"].encode('unicode-escape'),
+								" / ".join(event["references"])[:500],
+								self.separator)
 
-                        if indicator["type"] in ('domain', 'hostname', 'IPv4', 'IPv6'):
+						if indicator["type"] in ('domain', 'hostname', 'IPv4', 'IPv6'):
 
-                            for domain in DOMAIN_WHITELIST:
-                                if domain in indicator["indicator"]:
-                                    print indicator["indicator"]
-                                    raise WhiteListedIOC
+							for domain in DOMAIN_WHITELIST:
+								if domain in indicator["indicator"]:
+									print indicator["indicator"]
+									raise WhiteListedIOC
 
-                            self.c2_iocs += "{0}{3}{1} {2}\n".format(
-                                indicator["indicator"],
-                                event["name"].encode('unicode-escape'),
-                                " / ".join(event["references"])[:500],
-                                self.separator)
-                            
-                        if indicator["type"] in ('domain', 'hostname'):                      
-                          
-                             for domain in DOMAIN_WHITELIST:
-                                if domain in indicator["indicator"]:
-                                    print indicator["indicator"]
-                                    raise WhiteListedIOC
+							self.c2_iocs += "{0}{3}{1} {2}\n".format(
+								indicator["indicator"],
+								event["name"].encode('unicode-escape'),
+								" / ".join(event["references"])[:500],
+								self.separator)
 
-                            self.c2_domain_iocs += "{0}{3}{1} {2}\n".format(
-                                indicator["indicator"],
-                                event["name"].encode('unicode-escape'),
-                                " / ".join(event["references"])[:500],
-                                self.separator)
-                        
-                        if indicator["type"] in ('IPv4', 'IPv6'):                      
-                          
-                             for domain in DOMAIN_WHITELIST:
-                                if domain in indicator["indicator"]:
-                                    print indicator["indicator"]
-                                    raise WhiteListedIOC
+						if indicator["type"] in ('domain', 'hostname'):                      
 
-                            self.c2_ip_iocs += "{0}{3}{1} {2}\n".format(
-                                indicator["indicator"],
-                                event["name"].encode('unicode-escape'),
-                                " / ".join(event["references"])[:500],
-                                self.separator)
+							for domain in DOMAIN_WHITELIST:
+								if domain in indicator["indicator"]:
+									print indicator["indicator"]
+									raise WhiteListedIOC
 
-                    except WhiteListedIOC, e:
-                        pass
+							self.c2_domain_iocs += "{0}{3}{1} {2}\n".format(
+								indicator["indicator"],
+								event["name"].encode('unicode-escape'),
+								" / ".join(event["references"])[:500],
+								self.separator)
 
-            except Exception, e:
-                traceback.print_exc()
+						if indicator["type"] in ('IPv4', 'IPv6'):
 
-        # Write to files
-        with open(hash_ioc_file, "w") as hash_fh:
-            if self.use_csv_header:
-                hash_fh.write('hash{0}description\n'.format(self.separator))
-            hash_fh.write(self.hash_iocs)
-            print "{0} hash iocs written to {1}".format(self.hash_iocs.count('\n'), hash_ioc_file)
-        with open(filename_ioc_file, "w") as fn_fh:
-            if self.use_csv_header:
-                fn_fh.write('filename{0}description\n'.format(self.separator))
-            fn_fh.write(self.filename_iocs)
-            print "{0} filename iocs written to {1}".format(self.filename_iocs.count('\n'), filename_ioc_file)
-        with open(c2_ioc_file, "w") as c2_fh:
-            if self.use_csv_header:
-                c2_fh.write('host{0}description\n'.format(self.separator))
-            c2_fh.write(self.c2_iocs)
-            print "{0} c2 iocs written to {1}".format(self.c2_iocs.count('\n'), c2_ioc_file)
-         
-        with open(c2_domain_ioc_file, "w") as c2_dfh:
-            if self.use_csv_header:
-                c2_dfh.write('host{0}description\n'.format(self.separator))
-            c2_dfh.write(self.c2_domain_iocs)
-            print "{0} c2 domain iocs written to {1}".format(self.c2_domain_iocs.count('\n'), c2_domain_ioc_file)
-                  
-         with open(c2_ip_ioc_file, "w") as c2_ifh:
-            if self.use_csv_header:
-                c2_ifh.write('host{0}description\n'.format(self.separator))
-            c2_ifh.write(self.c2_ip_iocs)
-            print "{0} c2 IP iocs written to {1}".format(self.c2_ip_iocs.count('\n'), c2_ip_ioc_file)
+							for domain in DOMAIN_WHITELIST:
+								if domain in indicator["indicator"]:
+									print indicator["indicator"]
+									raise WhiteListedIOC
+
+							self.c2_ip_iocs += "{0}{3}{1} {2}\n".format(
+								indicator["indicator"],
+								event["name"].encode('unicode-escape'),
+								" / ".join(event["references"])[:500],
+								self.separator)
+
+					except WhiteListedIOC, e:
+						pass
+
+			except Exception, e:
+				traceback.print_exc()
+
+		# Write to files
+		with open(hash_ioc_file, "w") as hash_fh:
+			if self.use_csv_header:
+				hash_fh.write('hash{0}description\n'.format(self.separator))
+			hash_fh.write(self.hash_iocs)
+			print "{0} hash iocs written to {1}".format(self.hash_iocs.count('\n'), hash_ioc_file)
+		with open(filename_ioc_file, "w") as fn_fh:
+			if self.use_csv_header:
+				fn_fh.write('filename{0}description\n'.format(self.separator))
+			fn_fh.write(self.filename_iocs)
+			print "{0} filename iocs written to {1}".format(self.filename_iocs.count('\n'), filename_ioc_file)
+		with open(c2_ioc_file, "w") as c2_fh:
+			if self.use_csv_header:
+				c2_fh.write('host{0}description\n'.format(self.separator))
+			c2_fh.write(self.c2_iocs)
+			print "{0} c2 iocs written to {1}".format(self.c2_iocs.count('\n'), c2_ioc_file)
+		with open(c2_domain_ioc_file, "w") as c2d_fh:
+			if self.use_csv_header:
+				c2d_fh.write('host{0}description\n'.format(self.separator))
+			c2d_fh.write(self.c2_domain_iocs)
+			print "{0} c2 domain iocs written to {1}".format(self.c2_domain_iocs.count('\n'), c2_domain_ioc_file)     
+		with open(c2_ip_ioc_file, "w") as c2i_fh:
+			if self.use_csv_header:
+				c2i_fh.write('host{0}description\n'.format(self.separator))
+			c2i_fh.write(self.c2_ip_iocs)
+			print "{0} c2 IP iocs written to {1}".format(self.c2_ip_iocs.count('\n'), c2_ip_ioc_file)
 
 
 def my_escape(string):
-    return re.sub(r'([\-\(\)\.\[\]\{\}\\\+])',r'\\\1',string)
+	return re.sub(r'([\-\(\)\.\[\]\{\}\\\+])',r'\\\1',string)
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='OTX IOC Receiver')
-    parser.add_argument('-k', help='OTX API key', metavar='APIKEY', default=OTX_KEY)
-    # parser.add_argument('-l', help='Time frame in days (default=30)', default=30)
-    parser.add_argument('-o', metavar='dir', help='Output directory', default='/opt/threat_feeds/otx/iocs/')
-    parser.add_argument('-p', metavar='proxy', help='Proxy server (e.g. http://proxy:8080 or '
-                                                    'http://user:pass@proxy:8080', default=None)
-    parser.add_argument('--verifycert', action='store_true', help='Verify the server certificate', default=False)
-    parser.add_argument('--siem', action='store_true', default=False, help='CSV Output for use in SIEM systems')
-    parser.add_argument('--debug', action='store_true', default=False, help='Debug output')
+	parser = argparse.ArgumentParser(description='OTX IOC Receiver')
+	parser.add_argument('-k', help='OTX API key', metavar='APIKEY', default=OTX_KEY)
+	# parser.add_argument('-l', help='Time frame in days (default=30)', default=30)
+	parser.add_argument('-o', metavar='dir', help='Output directory', default='/opt/threat_feeds/otx/iocs/')
+	parser.add_argument('-p', metavar='proxy', help='Proxy server (e.g. http://proxy:8080 or '
+													'http://user:pass@proxy:8080', default=None)
+	parser.add_argument('--verifycert', action='store_true', help='Verify the server certificate', default=False)
+	parser.add_argument('--siem', action='store_true', default=False, help='CSV Output for use in SIEM systems')
+	parser.add_argument('--debug', action='store_true', default=False, help='Debug output')
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    if len(args.k) != 64:
-        print "Set an API key in script or via -k APIKEY. Go to https://otx.alienvault.com create an account and get your own API key"
-        sys.exit(0)
+	if len(args.k) != 64:
+		print "Set an API key in script or via -k APIKEY. Go to https://otx.alienvault.com create an account and get your own API key"
+		sys.exit(0)
 
-    # Create a receiver
-    otx_receiver = OTXReceiver(api_key=args.k, siem_mode=args.siem, debug=args.debug, proxy=args.p)
+	# Create a receiver
+	otx_receiver = OTXReceiver(api_key=args.k, siem_mode=args.siem, debug=args.debug, proxy=args.p)
 
-    # Retrieve the events and store the IOCs
-    # otx_receiver.get_iocs_last(int(args.l))
-    otx_receiver.get_iocs_last()
+	# Retrieve the events and store the IOCs
+	# otx_receiver.get_iocs_last(int(args.l))
+	otx_receiver.get_iocs_last()
 
-    # Write IOC files
-    otx_receiver.write_iocs(ioc_folder=args.o)
+	# Write IOC files
+	otx_receiver.write_iocs(ioc_folder=args.o)
